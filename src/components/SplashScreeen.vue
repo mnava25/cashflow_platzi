2eec6c0fb7422c45d9f165bb9d175483bca2b04d
Moises Pablo Nava Sangüeza <mnava@kpogroup.bo>
2023-07-20 08:35:14 -0400

    [FEATURE] archivo splasscreen

---
+++ src/components/SplashScreeen.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+  .splashscreen {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 100vh;
+  }
+  h1 {
+    color: var(--brand-blue);
+  }
+  h1 span {
+    color: var(--brand-green);
+  }
+</style>
