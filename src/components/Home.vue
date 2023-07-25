5b928a3c90891cb431b8a65094e2fb8ce2acfa02
Moises Pablo Nava Sangüeza <mnava@kpogroup.bo>
2023-07-25 08:37:58 -0400

    [FEATURE] movimientos e historial

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,15 +3,33 @@
     <template #header>
       <Header></Header>
     </template>
+    <template #resume>
+      <Resume :label="'Ahorro Total'" :amount="amount" :total-amount="1000000">
+        <template #graphic></template>
+        <template #action></template>
+      </Resume>
+    </template>
+    <template #movements>
+      <Movements></Movements>
+    </template>
   </Layout>
 </template>
 <script>
 import Layout from "@/components/Layout.vue";
 import Header from "@/components/Header.vue";
+import Resume from "@/components/Resume/Index.vue";
+import Movements from "@/components/Movements.vue";
 export default {
   components: {
     Layout,
     Header,
+    Resume,
+    Movements,
+  },
+  data() {
+    return {
+      amount: null,
+    };
   },
 };
 </script>
